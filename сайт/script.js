319238a52e0326b742eb2d0376a259dca832890c
C0vpy4 <100491011+C0vpy4@users.noreply.github.com>
2022-11-18 21:56:42 +0300

    Update script.js

---
+++ сайт/script.js
@@ -0,0 +1,11 @@
+setTimeout(function(){
+	document.getElementById('footer').style.display = 'none';
+    document.getElementById('footer').style.background = 'black';
+},1)
+
+
+setTimeout(function() {
+    document.getElementById('preloader').style.visibility = 'hidden';
+    document.getElementById('footer').style.display = 'inline';
+
+},2000); 
