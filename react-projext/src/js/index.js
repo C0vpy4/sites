325c7c8875eab7b3bff564adaae87297984908de
C0vpy4 <100491011+C0vpy4@users.noreply.github.com>
2023-03-28 14:10:00 +0300

    Add files via upload

---
+++ react-projext/src/js/index.js
@@ -0,0 +1,10 @@
+const x = document.getElementById("reshetka");
+
+function Over(){
+    x.style.visibility = "visible";
+    console.log("Over");
+}
+
+function Out(){
+    x.style.visibility = "hidden";
+}
